d637d6fd56c82137dd9334512acc4af2db4cf10d
Bruno Kenji <gilga-mesh@hotmail.com>
2017-03-01 08:31:47 -0300

    configured handler app, routes and initial endpoints

--- src/checking_account/handler.clj
+++ src/checking_account/handler.clj
@@ -8,5 +8,43 @@
             [ring.util.response :refer [response]]
             [compojure.route :as route]))
 
+(defn- put-credit [account-number body]
+  (prn "put-credit says hello")
+  "put-credit says hello")
+
+(defn- put-debit [account-number body]
+  (prn "put-debit says hello")
+  "put-debit says hello")
+
+(defn- get-balance [account-number]
+  (prn "get-balance says hello")
+  "get-balance says hello")
+
+(defn- get-statements [account-number params]
+  (prn "get-statements says hello")
+  "get-statements says hello")
+
+(defn- get-debts [account-number]
+  (prn "get-debts says hello")
+  "get-debts says hello")
+
+(defroutes app-routes
+  (context "/:account" [account]
+    (PUT "/credit" {body :body} (response (put-credit account body)))
+    (PUT "/debit" {body :body} (response (put-debit account body)))
+    (GET "/balance" [] (response (get-balance account)))
+    (GET "/statements" {params :query-params} (response (get-statements account (keywordize-keys params))))
+    (GET "/debts" [] (response (get-debts account))))
+  (route/not-found (response {:message "Page not found", :status 404})))
+
+(defn wrap-log-request [handler]
+  (fn [req] ; return handler function
+    (println req) ; perform logging
+    (handler req))) ; pass the request through to the inner handler
+
 (def app
-  (prn "Hi"))
+  (-> app-routes
+    (wrap-log-request)
+    (wrap-json-body {:keywords? true})
+    (wrap-params)
+    (wrap-json-response)))
